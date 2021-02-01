665e50d82c77fdc4131f0b12bad61cd454812525
eecs151 Master <eecs151@imail.eecs.berkeley.edu>
2021-02-01 10:52:06 -0800

    update fir.v to use register library

--- src/fir.v
+++ src/fir.v
@@ -1,27 +1,22 @@
+`include EECS151.v
+
 module fir #(
     parameter NUM_INPUT_BITS = 4,
     parameter NUM_OUTPUT_BITS = 16
 )  (
     input clk,
+    input rst,
     input signed [NUM_INPUT_BITS-1:0] In,
     output signed [NUM_OUTPUT_BITS-1:0] Out );
 
 
-reg signed [NUM_INPUT_BITS-1:0] delay_chain0, delay_chain1, delay_chain2, delay_chain3, delay_chain4;
-initial begin
-    delay_chain0 <= 0;
-    delay_chain1 <= 0;
-    delay_chain2 <= 0;
-    delay_chain3 <= 0;
-    delay_chain4 <= 0;
-end
-always @(posedge clk) begin
-    delay_chain4 <= delay_chain3;
-    delay_chain3 <= delay_chain2;
-    delay_chain2 <= delay_chain1;
-    delay_chain1 <= delay_chain0;
-    delay_chain0 <= In;
-end
+wire signed [NUM_INPUT_BITS-1:0] delay_chain0, delay_chain1, delay_chain2, delay_chain3, delay_chain4;
+
+REGISTER_R #(.N(NUM_INPUT_BITS)) delay_step0 (.clk(clk), .rst(rst), .d(In), .q(delay_chain0));
+REGISTER_R #(.N(NUM_INPUT_BITS)) delay_step1 (.clk(clk), .rst(rst), .d(delay_chain0), .q(delay_chain1));
+REGISTER_R #(.N(NUM_INPUT_BITS)) delay_step1 (.clk(clk), .rst(rst), .d(delay_chain1), .q(delay_chain2));
+REGISTER_R #(.N(NUM_INPUT_BITS)) delay_step1 (.clk(clk), .rst(rst), .d(delay_chain2), .q(delay_chain3));
+REGISTER_R #(.N(NUM_INPUT_BITS)) delay_step1 (.clk(clk), .rst(rst), .d(delay_chain3), .q(delay_chain4));
 
 addertree #(.NUM_INPUT_BITS(NUM_INPUT_BITS), .NUM_OUTPUT_BITS(NUM_OUTPUT_BITS)) add0 (
     .in0(delay_chain0), 
